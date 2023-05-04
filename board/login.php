8bde0207f6be65bd47c0317f44b89226d38beac1
Cafino007 <132442293+Cafino007@users.noreply.github.com>
2023-05-04 14:50:29 +0800

    Login Modification

--- board/login.php
+++ board/login.php
@@ -39,7 +39,7 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome test!</h1>
                                     </div>
                                     <?php
                                     session_start();
